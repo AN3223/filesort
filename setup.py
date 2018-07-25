29a6bdf5809f4486dd0f22a3bf784a1e56572a35
AN3223 <35916076+AN3223@users.noreply.github.com>
2018-07-24 19:22:38 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='filesort',
+    version='0.1.0',
+    description='A simple command line tool for sorting files',
+    url='https://github.com/AN3223/filesort',
+    author='AN3223',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['build', 'doc', 'dist', 'contrib', 'docs']),
+    entry_points={
+        'console_scripts': [
+            'filesort = filesort.filesort:main'
+        ]
+    }
+)
