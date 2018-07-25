f4645deb9e44d6bd2c424940180d7f21399da653
AN3223 <35916076+AN3223@users.noreply.github.com>
2018-07-24 19:30:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='filesort',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple command line tool for sorting files',
     url='https://github.com/AN3223/filesort',
     author='AN3223',
